112ea154f4e625046df5e20333ffd34f7d913c18
Marius <marius@myidea.fr>
2022-05-10 16:10:17 +0200

    add button to login

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'react-native'
+
+const CustomButton = (props) => {
+  return (
+    <Button {...props} />
+  )
+}
+
+export default CustomButton
--- src/components/form/LoginForm.js
+++ src/components/form/LoginForm.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import Input from './Input'
+import Button from '../Button'
 
 const LoginForm = () => {
   const [credentials, setCredentials] = useState({
     identifier: '',
     password: ''
   })
+
+  const handlePress = () => {
+    console.log('Bouton pressé !')
+  }
 
   return (
     <View>
@@ -21,6 +26,10 @@ const LoginForm = () => {
         onChangeText={(text) => setCredentials({ ...credentials, password: text })}
         value={credentials.password}
       />
+      <Button title='Se connecter' onPress={handlePress} />
+      <View>
+        <Text>{JSON.stringify(credentials, null, 2)}</Text>
+      </View>
     </View>
   )
 }
