d30f3dc0828cb66186d279812a1c81cdc3d1ed47
Marius <marius@myidea.fr>
2022-05-12 17:04:58 +0200

    add camera and one signal

--- index.js
+++ index.js
@@ -2,8 +2,35 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './src/App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native'
+import App from './src/App'
+import { name as appName } from './app.json'
+import OneSignal from 'react-native-onesignal'
 
-AppRegistry.registerComponent(appName, () => App);
+// OneSignal Init Code
+OneSignal.setLogLevel(6, 0)
+OneSignal.setAppId('9d557cd0-9325-4ae2-9226-8172e235717d')
+// END OneSignal Init Code
+
+// Prompt for push on iOS
+OneSignal.promptForPushNotificationsWithUserResponse(response => {
+  console.log('Prompt response:', response)
+})
+
+// Method for handling notifications received while app in foreground
+OneSignal.setNotificationWillShowInForegroundHandler(notificationReceivedEvent => {
+  console.log('OneSignal: notification will show in foreground:', notificationReceivedEvent)
+  const notification = notificationReceivedEvent.getNotification()
+  console.log('notification: ', notification)
+  const data = notification.additionalData
+  console.log('additionalData: ', data)
+  // Complete with null means don't show a notification.
+  notificationReceivedEvent.complete(notification)
+})
+
+// Method for handling notifications opened
+OneSignal.setNotificationOpenedHandler(notification => {
+  console.log('OneSignal: notification opened:', notification)
+})
+
+AppRegistry.registerComponent(appName, () => App)
